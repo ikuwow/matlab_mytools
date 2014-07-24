1369629c6ff43e984204c37f209a9ffd359ed6b1
Ikuo Degawa <degawa@ikuwow.com>
2014-07-24 17:05:20 +0900

    include OMP script

--- testMP.m
+++ testMP.m
@@ -3,6 +3,9 @@
 % The test code for MP.m
 % 
 
+clear all;
+close all;
+
 n = 400; m = 500;
 % d = rand(n,m);
 d = generateOrthogonalDCTBasis(20);
@@ -11,7 +14,9 @@ Dictionary = normc(d); % normalize
 y = rand(400,1);
 % y = (1:100)';
 
-[y_fit, s, debug] = MP(y,Dictionary,30);
+% [y_fit, s, debug] = MP(y,Dictionary,30);
+s = OMP(Dictionary,y,30);
+y_fit = Dictionary*s;
 
 figure(1)
 plot(1:length(y),y,1:length(y_fit),y_fit)
