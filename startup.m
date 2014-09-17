c75e0a6075b0385a526f7787cd3f6acbee7cbaea
Ikuo Degawa <degawa@ikuwow.com>
2014-09-17 11:59:02 +0900

    modify startup script

--- startup.m
+++ startup.m
@@ -1,6 +1,6 @@
 % addpath('~/Documents/MATLAB/matlab-midi-master/src');
 addpath('~/Documents/MATLAB/MyTools');
-addpath('~/Documents/MATLAB/AutoShared/Vendor');
+addpath('~/Documents/MATLAB/AutoShared/Libraries');
 % addpath('~/Documents/MATLAB/3Dimage_engineering/');
 % addpath('~/Documents/MATLAB/Bilateral_Filtering/');
 % addpath('~/Documents/MATLAB/multipitch_estimation/');
@@ -11,9 +11,9 @@ addpath('~/Documents/MATLAB/AutoShared/Vendor');
 addpath(genpath('~/Documents/MATLAB/Inpainting'));
 
 %% xlwrite.m
-% javaaddpath('Libraries/poi_library/poi-3.8-20120326.jar');
-% javaaddpath('Libraries/poi_library/poi-ooxml-3.8-20120326.jar');
-% javaaddpath('Libraries/poi_library/poi-ooxml-schemas-3.8-20120326.jar');
-% javaaddpath('Libraries/poi_library/xmlbeans-2.3.0.jar');
-% javaaddpath('Libraries/poi_library/dom4j-1.6.1.jar');
-% javaaddpath('Libraries/poi_library/stax-api-1.0.1.jar');
+javaaddpath('AutoShared/Libraries/poi_library/poi-3.8-20120326.jar');
+javaaddpath('AutoShared/Libraries/poi_library/poi-ooxml-3.8-20120326.jar');
+javaaddpath('AutoShared/Libraries/poi_library/poi-ooxml-schemas-3.8-20120326.jar');
+javaaddpath('AutoShared/Libraries/poi_library/xmlbeans-2.3.0.jar');
+javaaddpath('AutoShared/Libraries/poi_library/dom4j-1.6.1.jar');
+javaaddpath('AutoShared/Libraries/poi_library/stax-api-1.0.1.jar');
