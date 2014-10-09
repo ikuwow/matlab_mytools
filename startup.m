04a7844590eb5df35844f7cfebeac4fa3ac54b71
Ikuo Degawa <degawa@ikuwow.com>
2014-10-09 15:31:03 +0900

    Add custom mATLAB path

--- startup.m
+++ startup.m
@@ -17,3 +17,5 @@ javaaddpath('AutoShared/Libraries/poi_library/poi-ooxml-schemas-3.8-20120326.jar
 javaaddpath('AutoShared/Libraries/poi_library/xmlbeans-2.3.0.jar');
 javaaddpath('AutoShared/Libraries/poi_library/dom4j-1.6.1.jar');
 javaaddpath('AutoShared/Libraries/poi_library/stax-api-1.0.1.jar');
+
+setenv('PATH',['/usr/locla/bin',getenv('PATH')])
