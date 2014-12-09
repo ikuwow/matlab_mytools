ca259c0ecf897df4450a4524eba73cbc6ae27ef1
Ikuo Degawa <degawa@ikuwow.com>
2014-12-09 18:07:31 +0900

    remove redundant libraries from startup

--- startup.m
+++ startup.m
@@ -1,14 +1,5 @@
-% addpath('~/Documents/MATLAB/matlab-midi-master/src');
 addpath('~/Documents/MATLAB/MyTools');
 addpath('~/Documents/MATLAB/AutoShared/Libraries');
-% addpath('~/Documents/MATLAB/3Dimage_engineering/');
-% addpath('~/Documents/MATLAB/Bilateral_Filtering/');
-% addpath('~/Documents/MATLAB/multipitch_estimation/');
-% addpath(genpath('~/Documents/MATLAB/SR/ScSR'));
-% addpath(genpath('~/Documents/MATLAB/Libraries'));
-
-% inpainting
-addpath(genpath('~/Documents/MATLAB/Inpainting'));
 
 %% xlwrite.m
 javaaddpath('AutoShared/Libraries/poi_library/poi-3.8-20120326.jar');
